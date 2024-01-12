084a6deb0e7cb8fb7a6ad07c0b38bde0719add08
NikeBrumer <Nowllinks@yandex.ru>
2024-01-12 21:34:42 +0300

    added column 'date'

--- functions.py
+++ functions.py
@@ -1,9 +1,11 @@
 def returns_sheet(soup, work_sheet):
+    from datetime import datetime
     items = soup.find_all(class_='serp-item')
     for i in items:
         name = i.find(class_='serp-item__title').text
-        url = i.find(class_='serp-item__title').attrs['href']
+        url = i.find(class_='bloko-link').attrs['href']
         price = i.find(attrs={'data-qa': 'vacancy-serp__vacancy-compensation'})
+        date = datetime.now()
         if price is not None:
             price = price.text.replace('EUR', '€')
             price = price.replace('USD', '$')
@@ -11,7 +13,7 @@ def returns_sheet(soup, work_sheet):
             price = ''.join([i for i in price if i.isdigit() or (i in ['–', '€', '$', '₽'])])
         else:
             price = 'не указано'
-        row = [name, url, price]
+        row = [name, url, price, date]
         work_sheet.append(row)
     return work_sheet
 
